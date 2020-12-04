21ab739a0dc275d3566e17cd5209ef48526039e8
Patience Dendy <dendypat@gmail.com>
2020-12-03 19:16:31 -0500

    Inital Commit

---
+++ index.js
@@ -0,0 +1,15 @@
+
+$document.ready(function () {
+
+  document.getElementById("one").addEventListener("click", one);
+
+  function one () {
+    document.getElementById("one").innerHTML = 1,
+  }
+
+  // function clear () {
+  //   document.getElementById("display").reset();
+  // }
+
+
+})
